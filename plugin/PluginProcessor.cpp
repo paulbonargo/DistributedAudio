e65db417022c91e7501fcf4f8132fa8af55cb17a
paulbonargo <paulbonjr@gmail.com>
2026-06-07 16:52:47 -0400

    added core juce plugin files ; created plugin processor - without any funcitonality yet, just so a DAW could recognize the plugin

---
+++ plugin/PluginProcessor.cpp
@@ -0,0 +1,55 @@
+/*
+  ==============================================================================
+
+    This file contains the basic framework code for a JUCE plugin processor.
+
+  ==============================================================================
+*/
+
+#include "PluginProcessor.h"
+#include "PluginEditor.h"
+
+//==============================================================================
+
+AudioSenderProcessor::AudioSenderProcessor() : juce::AudioProcessor(BusesProperties()
+    .withInput("Input", juce::AudioChannelSet::stereo(), true)
+    .withOutput("Output", juce::AudioChannelSet::stereo(), true))
+{
+}
+
+void AudioSenderProcessor::prepareToPlay(double, int) 
+{
+
+}
+
+bool AudioSenderProcessor::isBusesLayoutSupported(const BusesLayout& layouts) const
+{
+    // accept mono or stereo - as long as if output type matches input type
+    const auto& out = layouts.getMainOutputChannelSet();
+    if (out != juce::AudioChannelSet::mono() && out != juce::AudioChannelSet::stereo())
+        return false;
+
+    return layouts.getMainInputChannelSet() == out;
+}
+
+void AudioSenderProcessor::processBlock(juce::AudioBuffer<float>& buffer, juce::MidiBuffer&)
+{
+    juce::ScopedNoDenormals noDenormals;
+
+    // clear any input channel higher than max output channel count
+    for (int channel = getTotalNumInputChannels(); channel < getTotalNumOutputChannels(); ++channel)
+		buffer.clear(channel, 0, buffer.getNumSamples());
+
+    // networking code to send audio data to receiver will be added here
+
+}
+
+juce::AudioProcessorEditor* AudioSenderProcessor::createEditor()
+{
+	
+}
+
+juce::AudioProcessor* JUCE_CALLTYPE createPluginFilter()
+{
+    return new AudioSenderProcessor();
+}
